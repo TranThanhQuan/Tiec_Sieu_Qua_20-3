058dff45c7555be8ed7ddcadd2484e107f2a7311
Nguyễn Thanh Tân <tannt@appota.com>
2022-03-16 11:31:56 +0700

    update scipt index

--- assets/js/script_index.js
+++ assets/js/script_index.js
@@ -21,78 +21,88 @@ $(document).ready(function() {
 
 
 function getDataClient (){
+    $.ajax({
+        url : 'http://loanthe.gamota/api/getDataClient.php',
+        type: 'POST',
+        data: 'allow='+1,
+        dataType: 'json',
+        beforeSend:function(){
+            // show_dice();
+        },
+        success: function(data){
 
-    $.getJSON('http://loanthe.gamota/api/getDataClient.php', function(data) {
-
-
-        let exchange_award = data.data.exchange_award;
-        $.each(exchange_award, function(key, value){
-            let sent = `Đã đổi ${value.sent}/${value.max}`;
-            let id_addr = `${key+1}`;
-            let diemdoi = `${value.req} điểm đổi`;            
-            $("#doiqua-sent"+id_addr).html(sent);
-            $("#diemdoi"+id_addr).html(diemdoi);
-            
-
-            var dadoi;
-            if (value.sent < value.max) {
+            let exchange_award = data.data.exchange_award;
+            $.each(exchange_award, function(key, value){
+                let sent = `Đã đổi ${value.sent}/${value.max}`;
+                let id_addr = `${key+1}`;
+                let diemdoi = `${value.req} điểm đổi`;            
+                $("#doiqua-sent"+id_addr).html(sent);
+                $("#diemdoi"+id_addr).html(diemdoi);
                 
-                dadoi = `<img src="style/img/nhanqua-btn.png" class="bottom__card-btn" alt="">`;
+
+                var dadoi;
+                if (value.sent < value.max) {
+                    
+                    dadoi = `<img src="style/img/nhanqua-btn.png" class="bottom__card-btn" alt="">`;
+                }
+                else{
+                    dadoi = `<img src="style/img/danhan-btn.png" class="bottom__card-btn" alt="">`;
+                }
+                $("#doiqua-btn"+id_addr).html(dadoi);
+
+            });
+
+
+
+            // login
+            let user_login = data.data.user_login;
+            console.log(user_login);
+
+            if(user_login.is_login == 0){
+                $('#dangnhap').attr("class", "show");
+                $('#dangnhap-mob').attr("class","show-navbar");
+
+                // Nếu chưa đăng nhập thì lượt tặng hoa = 0
+                let luottanghoa = `Bạn có: 0 Lượt Tặng`;
+                $("#luottanghoa").html(luottanghoa);
+                $("#luottang-mob").html(luottanghoa);
+
+                
+
+                // Nếu chưa đăng nhập thì điểm thân mật = 0
+                let diemthanmat = `Bạn có: 0 Điểm thân mật`;
+                $("#diemthanmat").html(diemthanmat);
+                $("#diemthanmat-top").html(diemthanmat);
+                $("#diemthanmat-bottom").html(diemthanmat);
+
+                console.log("1");
             }
+
             else{
-                dadoi = `<img src="style/img/danhan-btn.png" class="bottom__card-btn" alt="">`;
+                let username = user_login.username;
+                $('#dangxuat').attr("class", "show");
+                $('#dangxuat-mob').attr("class","show-navbar");
+                $("#username").html(username);
+                
+                let luottanghoa = `Bạn có: ${data.data.turn_remain} Lượt Tặng`;
+                $("#luottanghoa").html(luottanghoa);
+                $("#luottang-mob").html(luottanghoa);
+
+                
+                let diemthanmat = `Bạn có: ${data.data.point} Điểm thân mật`;
+                $("#diemthanmat").html(diemthanmat);
+                $("#diemthanmat-top").html(diemthanmat);
+                $("#diemthanmat-bottom").html(diemthanmat);
+
+
+                console.log("2");
             }
-            $("#doiqua-btn"+id_addr).html(dadoi);
-
-        });
-
-
-
-        // login
-        let user_login = data.data.user_login;
-        console.log(user_login);
-
-        if(user_login.is_login == 0){
-            $('#dangnhap').attr("class", "show");
-            $('#dangnhap-mob').attr("class","show-navbar");
-
-            // Nếu chưa đăng nhập thì lượt tặng hoa = 0
-            let luottanghoa = `Bạn có: 0 Lượt Tặng`;
-            $("#luottanghoa").html(luottanghoa);
-            $("#luottang-mob").html(luottanghoa);
-
-            
-
-            // Nếu chưa đăng nhập thì điểm thân mật = 0
-            let diemthanmat = `Bạn có: 0 Điểm thân mật`;
-            $("#diemthanmat").html(diemthanmat);
-            $("#diemthanmat-top").html(diemthanmat);
-            $("#diemthanmat-bottom").html(diemthanmat);
-
-            console.log("1");
-        }
-
-        else{
-            let username = user_login.username;
-            $('#dangxuat').attr("class", "show");
-            $('#dangxuat-mob').attr("class","show-navbar");
-            $("#username").html(username);
-            
-            let luottanghoa = `Bạn có: ${data.data.turn_remain} Lượt Tặng`;
-            $("#luottanghoa").html(luottanghoa);
-            $("#luottang-mob").html(luottanghoa);
-
-            
-            let diemthanmat = `Bạn có: ${data.data.point} Điểm thân mật`;
-            $("#diemthanmat").html(diemthanmat);
-            $("#diemthanmat-top").html(diemthanmat);
-            $("#diemthanmat-bottom").html(diemthanmat);
-
-
-            console.log("2");
+        },
+        complete: function(){
+            // loadDataClient();
         }
     });
-};
+}
 
 
 
